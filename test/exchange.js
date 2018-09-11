2caba7782036a5f35a523c721f65fbb50a05ee23
PhilipPfeffer <philpfef@stanford.edu>
2018-09-11 11:02:02 +0100

    Exchange test for full order

--- test/exchange.js
+++ test/exchange.js
@@ -1,4 +1,7 @@
 /* global artifacts, assert, contract, beforeEach, it:true */
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545'));
+
 const Exchange = artifacts.require('./contracts/exchange/Exchange');
 const UsefulCoin = artifacts.require('./contracts/tokens/UsefulCoin');
 const SecurityTest = artifacts.require('./contracts/Security/SecurityTest');
@@ -6,7 +9,7 @@ const ecdsaApi = require('../helpers/ecdsaApi');
 const exceptions = require('./exceptions');
 const coordsContract = artifacts.require('./FOO.sol');
 
-
+// https://ethereum.stackexchange.com/questions/8650/is-there-a-maximum-number-of-entries-for-a-mapping
 contract.only('Token', (accounts) => {
     let arethaFrankloans;
     let andollarsPaak;
@@ -17,82 +20,75 @@ contract.only('Token', (accounts) => {
     let coordsCon;
 
     beforeEach(async () => {
-        arethaFrankloans = await UsefulCoin.new();
-        andollarsPaak = await UsefulCoin.new();
-        witherBills = await UsefulCoin.new();
-        smoniy = await UsefulCoin.new();
+        arethaFrankloans = await UsefulCoin.new({ from: accounts[0], value: web3.toWei("1", "ether")});
+        andollarsPaak = await UsefulCoin.new({ from: accounts[1], value: web3.toWei("2", "ether")});
+        witherBills = await UsefulCoin.new({ from: accounts[2], value: web3.toWei("3", "ether")});
+        smoniy = await UsefulCoin.new({ from: accounts[3], value: web3.toWei("4", "ether")});
         securityTest = await SecurityTest.new({
             from: accounts[0],
         });
         dexchange = await Exchange.new();
-
-        // for some reason when i have different accounts in {from: }, they still only go to account[0]
-        await arethaFrankloans.send(web3.toWei("1", "ether"), { from: accounts[0] });
-        await andollarsPaak.send(web3.toWei("2", "ether"), { from: accounts[0] });
-        await witherBills.send(web3.toWei("3", "ether"), { from: accounts[0] });
-        await smoniy.send(web3.toWei("4", "ether"), { from: accounts[0] });
-
         coordsCon = await coordsContract.new();
     });
     
     it('initial balances are correct', async () => {
         const balance0 = await arethaFrankloans.balanceOf(accounts[0]);
-        const balance4 = await arethaFrankloans.balanceOf(accounts[1]);
-        const balance1 = await andollarsPaak.balanceOf(accounts[0]);
-        const balance2 = await witherBills.balanceOf(accounts[0]);
-        const balance3 = await smoniy.balanceOf(accounts[0]);
+        const balance1 = await andollarsPaak.balanceOf(accounts[1]);
+        const balance2 = await witherBills.balanceOf(accounts[2]);
+        const balance3 = await smoniy.balanceOf(accounts[3]);
         assert.equal(balance0.toString(10) , web3.toWei("1000000", "ether"));
         assert.equal(balance1.toString(10) , web3.toWei("2000000", "ether"));
         assert.equal(balance2.toString(10) , web3.toWei("3000000", "ether"));
         assert.equal(balance3.toString(10) , web3.toWei("4000000", "ether"));
-        assert.equal(balance4.toString(10) , web3.toWei("0", "ether"));
     });
 
-    it.only('test test', async () => {
-        // const coord0 = {x: 100, y: 200};
-        // const coords = [coord0, coord0, coord0];
-        // const worked = await coordsCon.loopCoords(coords);
-        const coord0 = await coordsCon.coordMaker(100, 200);
-        const worked = await coordsCon.justCoord(coord0);
+    it.only('succesfully executes exchange of assets - non partial order', async () => {
+        const maker = accounts[0];
+        const taker = accounts[1];
+        const makerValue = web3.toWei("700000", "ether");
+        const takerValue = web3.toWei("1500000", "ether");
+        const makerSignature = ecdsaApi.signMessageComplex({
+            callingAddress: dexchange.address,
+            value: makerValue,
+            nonce: 0,
+            sender: accounts[0],
+        });
+        const takerSignature = ecdsaApi.signMessageComplex({
+            callingAddress: dexchange.address,
+            value: takerValue,
+            nonce: 0,
+            sender: accounts[1],
+        });
+        const orderHash = ecdsaApi.encodeAndHash(makerSignature, takerSignature);
+
+        const orderAddresses = [maker, taker, arethaFrankloans.address, andollarsPaak.address];
+        const orderValues = [makerValue, takerValue, takerValue];
+        await dexchange.assembleOrder(
+            orderAddresses,
+            orderValues,
+            makerSignature.r, makerSignature.s, makerSignature.v,
+            takerSignature.r, takerSignature.s, takerSignature.v,
+            false,
+            orderHash,
+        );
+        
+        await dexchange.fillOrdersTemp();
+        
+        const balance0 = await arethaFrankloans.balanceOf(accounts[0]);
+        const balance1 = await arethaFrankloans.balanceOf(accounts[1]);
+        const balance2 = await andollarsPaak.balanceOf(accounts[0]);
+        const balance3 = await andollarsPaak.balanceOf(accounts[1]);
+        assert.equal(balance0.toString(10), web3.toWei("300000", "ether"));
+        assert.equal(balance1.toString(10), web3.toWei("700000", "ether"));
+        assert.equal(balance2.toString(10), web3.toWei("1500000", "ether"));
+        assert.equal(balance3.toString(10), web3.toWei("500000", "ether"));
+
+        // const orders = [order0];
+        // await dexchange.fillOrders(orders);
         assert.isTrue(false);
     });
 
-    // it.only('succesfully executes transfer of assets', async () => {
-    //     const maker = accounts[0];
-    //     const taker = accounts[1];
-    //     const makerValue = web3.toWei("1000000", "ether");
-    //     const takerValue = web3.toWei("2000000", "ether")
-    //     const makerSignature = ecdsaApi.signMessageComplex({
-    //         callingAddress: dexchange.address,
-    //         makerValue,
-    //         nonce: 0,
-    //         sender: accounts[0],
-    //     });
-    //     const takerSignature = ecdsaApi.signMessageComplex({
-    //         callingAddress: dexchange.address,
-    //         takerValue,
-    //         nonce: 0,
-    //         sender: accounts[1],
-    //     });
-    //     const orderHash = ecdsaApi.encodeAndHash(makerSignature, takerSignature);
-
-    //     const order0 = {
-    //         maker,
-    //         taker,
-    //         makerToken: arethaFrankloans.address,
-    //         takerToken: andollarsPaak.address,
-    //         makerTokenAmount: makerValue,
-    //         takerTokenRequested: takerValue,
-    //         takerTokenSupplied: takerValue,
-    //         makerSignature,
-    //         takerSignature,
-    //         fillingPartial: false,
-    //         orderHash,
-    //     }
-
-    //     const orders = [order0];
-
-    //     await dexchange.fillOrders(orders);
-    //     // assert.isTrue(false);
-    // });
+    it('unauthorised order causes revert', async () => {
+    
+    });
 });
