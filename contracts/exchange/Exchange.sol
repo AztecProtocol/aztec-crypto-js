0409c358a20fbdbcb4b9825acbe2d29e4d34f17c
PhilipPfeffer <philpfef@stanford.edu>
2018-09-12 17:09:48 +0100

    Partial order fill implementation

--- contracts/exchange/Exchange.sol
+++ contracts/exchange/Exchange.sol
@@ -23,7 +23,6 @@ contract Exchange {
         address makerToken; // smart contract address of maker asset
         address takerToken;
         uint256 makerTokenAmount;
-        uint256 takerTokenAmount;
         uint256 takerTokenRequested;
         uint256 takerTokenSupplied; // the taker might not completely fill the maker's order
         ECDSASignature makerSignature;
@@ -51,12 +50,12 @@ contract Exchange {
         owner = msg.sender; // We will want to upgrade this to use a more distributed governance mechanic, but this will work fine for now
     }
 
-    /// @dev Assembles a single order struct
+    /// @dev Assembles an Order struct
     /// @param orderAddresses is [maker, taker, makerToken, takerToken]
     /// @param orderValues is [makerTokenAmount, takerTokenRequested, takerTokenSupplied]
     function assembleOrder(
         address[4] orderAddresses,
-        uint256[4] orderValues,
+        uint256[3] orderValues,
         bytes32 makerSignatureR, bytes32 makerSignatureS, uint8 makerSignatureV,
         bytes32 takerSignatureR, bytes32 takerSignatureS, uint8 takerSignatureV,
         bool fillingPartial,
@@ -68,9 +67,8 @@ contract Exchange {
             makerToken: orderAddresses[2],
             takerToken: orderAddresses[3],
             makerTokenAmount: orderValues[0],
-            takerTokenAmount: orderValues[1],
-            takerTokenRequested: orderValues[2],
-            takerTokenSupplied: orderValues[3],
+            takerTokenRequested: orderValues[1],
+            takerTokenSupplied: orderValues[2],
             makerSignature: ECDSASignature(makerSignatureR, makerSignatureS, makerSignatureV),
             takerSignature: ECDSASignature(takerSignatureR, takerSignatureS, takerSignatureV),
             fillingPartial: fillingPartial,
@@ -88,29 +86,31 @@ contract Exchange {
         tokenRegistry[tokenAddress] = false;
     }
  
-    // function fillOrders(Order[] orders) public returns (bool) {
-    //     for (uint256 i = 0; i < orders.length; i++) {
-    //         Order memory currOrder = orders[i];
-    //         addToken(currOrder.makerToken);
-    //         addToken(currOrder.takerToken);
-    //         if (currOrder.fillingPartial && partialFills[currOrder.orderHash]) {
-    //             // must check that orders are not cancelled
-    //             UsefulCoin takerToken = UsefulCoin(currOrder.takerToken);
-    //             UsefulCoin makerToken = UsefulCoin(currOrder.makerToken);
-    //             takerToken.transferFrom(currOrder.taker, currOrder.maker, currOrder.takerTokenSupplied);
-    //             // how to ensure that the full amount has been paid by the order taker
-    //             makerToken.delegatedTransfer(
-    //                 currOrder.maker,
-    //                 currOrder.taker,
-    //                 currOrder.makerTokenAmount,
-    //                 currOrder.makerTokenAmount,
-    //                 currOrder.makerSignature.r,
-    //                 currOrder.makerSignature.s,
-    //                 currOrder.makerSignature.v);
-    //         } else if (!currOrder.fillingPartial) _newOrder(currOrder);
-    //         removeToken(currOrder.makerToken);
-    //         removeToken(currOrder.takerToken);
-    //     }
+    // function fillOrders(
+    //     address[][] allOrderAddresses,
+    //     uint256[][] allOrderValues,
+    //     bytes32[] allMakerSignatureR, bytes32[] allMakerSignatureS, uint8[] allMakerSignatureV,
+    //     bytes32[] allTakerSignatureR, bytes32[] allTakerSignatureS, uint8[] allTakerSignatureV,
+    //     bool[] allFillingPartial,
+    //     bytes32[] allOrderHash
+    // ) public returns (bool) {
+    //     emit DebugUint256(3); //allOrderAddresses[0].length);
+    //     // for (uint256 i = 0; i < allOrderHash.length; i++) {
+    //     //     _fillOrder(
+    //     //         Order({
+    //     //             maker: allOrderAddresses[i][0],
+    //     //             taker: allOrderAddresses[i][1],
+    //     //             makerToken: allOrderAddresses[i][2],
+    //     //             takerToken: allOrderAddresses[i][3],
+    //     //             makerTokenAmount: allOrderValues[i][0],
+    //     //             takerTokenRequested: allOrderValues[i][1],
+    //     //             takerTokenSupplied: allOrderValues[i][2],
+    //     //             makerSignature: ECDSASignature(allMakerSignatureR[i], allMakerSignatureS[i], allMakerSignatureV[i]),
+    //     //             takerSignature: ECDSASignature(allTakerSignatureR[i], allTakerSignatureS[i], allTakerSignatureV[i]),
+    //     //             fillingPartial: allFillingPartial[i],
+    //     //             orderHash: allOrderHash[i]
+    //     //         }));
+    //     // }
     // }
 
     function fillOrdersTemp() public returns (bool) {
@@ -120,45 +120,58 @@ contract Exchange {
         }
     }
 
-    function _fillOrder(Order order) public returns (bool) {
+    function _fillOrder(Order order) internal returns (bool) {
         addToken(order.makerToken);
         addToken(order.takerToken);
-        if (order.fillingPartial && partialFills[order.orderHash]) {
-            // must check that orders are not cancelled
+        if (!order.fillingPartial) _newOrder(order);
+        else {
+            require(partialFills[order.orderHash], "The partial order you are trying to fill is invalid");
+            require(!cancelledOrders[order.orderHash], "This partial order has been cancelled");
             UsefulCoin takerToken = UsefulCoin(order.takerToken);
             UsefulCoin makerToken = UsefulCoin(order.makerToken);
-            takerToken.transferFrom(order.taker, order.maker, order.takerTokenSupplied);
-            // how to ensure that the full amount has been paid by the order taker
-            // increment supplied by amount
-            makerToken.delegatedTransfer(
-                order.maker, order.taker,
-                order.makerTokenAmount, order.makerTokenAmount,
-                order.makerSignature.r, order.makerSignature.s, order.makerSignature.v);
-        } else if (!order.fillingPartial) _newOrder(order);
+            uint256 makerToTransfer = order.makerTokenAmount*(boundedTakerToken/order.takerTokenRequested);
+            uint256 boundedTakerToken = order.takerTokenSupplied;
+            if (order.takerTokenSupplied > order.takerTokenRequested) boundedTakerToken = order.takerTokenRequested;
+        
+            takerToken.transferFrom(order.taker, order.maker, boundedTakerToken);
+            makerToken.transferFrom(order.maker, order.taker, makerToTransfer);
+            delete partialFills[order.orderHash];
+            partialFills[keccak256(
+                abi.encode(
+                    order.maker, order.makerToken, order.takerToken,
+                    order.makerTokenAmount - makerToTransfer,
+                    order.takerTokenRequested - order.takerTokenSupplied
+            ))] = true;
+        }
         removeToken(order.makerToken);
         removeToken(order.takerToken);
         return true;
     }
 
-    function _newOrder(Order order) public returns (bool) {
+    function _newOrder(Order order) internal returns (bool) {
         UsefulCoin takerToken = UsefulCoin(order.takerToken);
         UsefulCoin makerToken = UsefulCoin(order.makerToken);
-        if (order.takerTokenAmount < order.takerTokenRequested) {
-            partialFills[order.orderHash] = true;
-            takerToken.delegatedTransfer(
-                order.taker, order.maker,
-                order.takerTokenAmount, order.takerTokenRequested,
-                order.takerSignature.r, order.takerSignature.s, order.takerSignature.v);
-            order.takerTokenSupplied += order.takerTokenAmount;
-        } else {
-            takerToken.delegatedTransfer(
-                order.taker, order.maker,
-                order.takerTokenRequested, order.takerTokenRequested,
-                order.takerSignature.r, order.takerSignature.s, order.takerSignature.v);
-            makerToken.delegatedTransfer(
-                order.maker, order.taker,
-                order.makerTokenAmount, order.makerTokenAmount,
-                order.makerSignature.r, order.makerSignature.s, order.makerSignature.v);
+
+        uint256 makerToTransfer = order.makerTokenAmount*(boundedTakerToken/order.takerTokenRequested);
+        uint256 boundedTakerToken = order.takerTokenSupplied;
+        if (order.takerTokenSupplied > order.takerTokenRequested) boundedTakerToken = order.takerTokenRequested;
+        
+        takerToken.delegatedTransfer(
+            order.taker, order.maker,
+            boundedTakerToken, order.takerTokenRequested,
+            order.takerSignature.r, order.takerSignature.s, order.takerSignature.v);
+        makerToken.delegatedTransfer(
+            order.maker, order.taker,
+            makerToTransfer, order.makerTokenAmount,
+            order.makerSignature.r, order.makerSignature.s, order.makerSignature.v);
+
+        if (order.takerTokenSupplied < order.takerTokenRequested) {
+            partialFills[keccak256(
+                abi.encode(
+                    order.maker, order.makerToken, order.takerToken,
+                    order.makerTokenAmount - makerToTransfer,
+                    order.takerTokenRequested - order.takerTokenSupplied
+            ))] = true;
         }
         return true;
     }
--- test/exchange.js
+++ test/exchange.js
@@ -33,33 +33,34 @@ contract.only('Token', (accounts) => {
         assert.equal(balance3.toString(10) , web3.toWei("4000000", "ether"));
     });
 
-    it.only('succesfully executes exchange of assets - non partial order', async () => {
+    it('succesfully executes exchange of assets - non partial order', async () => {
         const maker = accounts[0];
         const taker = accounts[1];
+        const makerToken = arethaFrankloans.address;
+        const takerToken = andollarsPaak.address;
         const makerValue = web3.toWei("700000", "ether");
         const takerValue = web3.toWei("1500000", "ether");
         const makerSignature = ecdsaApi.signMessageComplex({
             callingAddress: dexchange.address,
             value: makerValue,
             nonce: 0,
-            sender: accounts[0],
+            sender: maker,
         });
         const takerSignature = ecdsaApi.signMessageComplex({
             callingAddress: dexchange.address,
             value: takerValue,
             nonce: 0,
-            sender: accounts[1],
+            sender: taker,
         });
-        const orderHash = ecdsaApi.encodeAndHash(makerSignature, takerSignature);
-        const orderAddresses = [maker, taker, arethaFrankloans.address, andollarsPaak.address];
-        const orderValues = [makerValue, takerValue, takerValue, takerValue];
-        const orderIndex = await dexchange.assembleOrder(
-            orderAddresses,
-            orderValues,
+
+        const orderHash = ecdsaApi.encodeAndHash(maker, makerToken, takerToken, makerValue, takerValue);
+        const orderAddresses = [maker, taker, makerToken, takerToken];
+        const orderValues = [makerValue, takerValue, takerValue];
+        await dexchange.assembleOrder(
+            orderAddresses, orderValues,
             makerSignature.r, makerSignature.s, makerSignature.v,
             takerSignature.r, takerSignature.s, takerSignature.v,
-            false,
-            orderHash,
+            false, orderHash,
         );
         
         await dexchange.fillOrdersTemp();
@@ -72,10 +73,105 @@ contract.only('Token', (accounts) => {
         assert.equal(balance1.toString(10), web3.toWei("700000", "ether"));
         assert.equal(balance2.toString(10), web3.toWei("1500000", "ether"));
         assert.equal(balance3.toString(10), web3.toWei("500000", "ether"));
-        assert.isTrue(false);
     });
 
     it('unauthorised order causes revert', async () => {
-    
+        const maker = accounts[0];
+        const taker = accounts[1];
+        const makerValue = web3.toWei("700000", "ether");
+        const takerValue = web3.toWei("1500000", "ether");
+        const makerSignature = ecdsaApi.signMessageComplex({
+            callingAddress: 0xdeadbeefdeadbeef,
+            value: makerValue,
+            nonce: 0,
+            sender: maker,
+        });
+        const takerSignature = ecdsaApi.signMessageComplex({
+            callingAddress: dexchange.address,
+            value: takerValue,
+            nonce: 0,
+            sender: taker,
+        });
+        const orderHash = ecdsaApi.encodeAndHash(makerSignature, takerSignature);
+        const orderAddresses = [maker, taker, arethaFrankloans.address, andollarsPaak.address];
+        const orderValues = [makerValue, takerValue, takerValue];
+        await dexchange.assembleOrder(
+            orderAddresses,
+            orderValues,
+            makerSignature.r, makerSignature.s, makerSignature.v,
+            takerSignature.r, takerSignature.s, takerSignature.v,
+            false,
+            orderHash,
+        );
+        
+        await exceptions.catchRevert(dexchange.fillOrdersTemp());
     });
+
+    // it.only('USING PROPER FILL ORDER succesfully executes exchange of assets - non partial order', async () => {
+    //     const maker = accounts[0];
+    //     const taker = accounts[1];
+    //     const makerValue = web3.toWei("700000", "ether");
+    //     const takerValue = web3.toWei("1500000", "ether");
+    //     const makerSignature = ecdsaApi.signMessageComplex({
+    //         callingAddress: dexchange.address,
+    //         value: makerValue,
+    //         nonce: 0,
+    //         sender: accounts[0],
+    //     });
+    //     const takerSignature = ecdsaApi.signMessageComplex({
+    //         callingAddress: dexchange.address,
+    //         value: takerValue,
+    //         nonce: 0,
+    //         sender: accounts[1],
+    //     });
+    //     const orderHash = ecdsaApi.encodeAndHash(makerSignature, takerSignature);
+    //     const orderAddresses = [maker, taker, arethaFrankloans.address, andollarsPaak.address];
+    //     const orderValues = [makerValue, takerValue, takerValue];
+    //     await dexchange.assembleOrder(
+    //         orderAddresses, orderValues,
+    //         makerSignature.r, makerSignature.s, makerSignature.v,
+    //         takerSignature.r, takerSignature.s, takerSignature.v,
+    //         false, orderHash,
+    //     );
+
+    //     const numOrders = 2;
+    //     let allOrderAddresses = [];
+    //     let allOrderValues = [];
+    //     let allMakerSignatureR = []; 
+    //     let allMakerSignatureS = []; 
+    //     let allMakerSignatureV = []; 
+    //     let allTakerSignatureR = []; 
+    //     let allTakerSignatureS = []; 
+    //     let allTakerSignatureV = [];
+    //     let allFillingPartial = [];
+    //     let allOrderHashes = [];
+    //     for (let i = 0; i < numOrders; i++) {
+    //         allOrderAddresses[i] = orderAddresses;
+    //         allOrderValues[i] = orderValues;
+    //         allMakerSignatureR[i] = makerSignature.r;
+    //         allMakerSignatureS[i] = makerSignature.s;
+    //         allMakerSignatureV[i] = makerSignature.v;
+    //         allTakerSignatureR[i] = takerSignature.r;
+    //         allTakerSignatureS[i] = takerSignature.s;
+    //         allTakerSignatureV[i] = takerSignature.v;
+    //         allFillingPartial[i] = false;
+    //         allOrderHashes[i] = orderHash;
+    //     }
+
+    //     await dexchange.fillOrders(
+    //         allOrderAddresses, allOrderValues, 
+    //         allMakerSignatureR, allMakerSignatureS, allMakerSignatureV, 
+    //         allTakerSignatureR, allTakerSignatureS, allTakerSignatureV,
+    //         allFillingPartial, allOrderHashes
+    //     );
+        
+    //     const balance0 = await arethaFrankloans.balanceOf(accounts[0]);
+    //     const balance1 = await arethaFrankloans.balanceOf(accounts[1]);
+    //     const balance2 = await andollarsPaak.balanceOf(accounts[0]);
+    //     const balance3 = await andollarsPaak.balanceOf(accounts[1]);
+    //     assert.equal(balance0.toString(10), web3.toWei("300000", "ether"));
+    //     assert.equal(balance1.toString(10), web3.toWei("700000", "ether"));
+    //     assert.equal(balance2.toString(10), web3.toWei("1500000", "ether"));
+    //     assert.equal(balance3.toString(10), web3.toWei("500000", "ether"));
+    // });
 });
