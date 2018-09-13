ec929946daf4e8e857f298d73dac96ead7f668ca
PhilipPfeffer <philpfef@stanford.edu>
2018-09-13 15:09:54 +0100

    test for trying to take tokens from already completed partial order

--- test/exchange.js
+++ test/exchange.js
@@ -36,7 +36,8 @@ contract.only('Token', (accounts) => {
         assert.equal(balance4.toString(10) , web3.toWei("1000000", "ether"));
     });
 
-    it('succesfully executes a valid non-partial order', async () => {
+    it('executes a valid non-partial order', async () => {
+        // Setup
         const maker = accounts[0];
         const taker = accounts[1];
         const makerToken = arethaFrankloans.address;
@@ -56,6 +57,7 @@ contract.only('Token', (accounts) => {
             sender: taker,
         });
 
+        // Full order
         const orderHash = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, makerValue, takerValue);
         const orderAddresses = [maker, taker, makerToken, takerToken];
         const orderValues = [makerValue, takerValue, takerValue];
@@ -77,7 +79,8 @@ contract.only('Token', (accounts) => {
         assert.equal(balance3.toString(10), web3.toWei("500000", "ether"));
     });
 
-    it('succesfully executes a valid partial order, same taker', async () => {
+    it('executes a valid partial order, same taker', async () => {
+        // Setup
         const maker = accounts[0];
         const taker = accounts[1];
         const makerToken = arethaFrankloans.address;
@@ -100,6 +103,7 @@ contract.only('Token', (accounts) => {
             sender: taker,
         });
 
+        // Part 1
         const orderHash1 = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, makerValue1, takerValue1);
         const orderAddresses = [maker, taker, makerToken, takerToken];
         const orderValues1 = [makerValue1, takerValue1, takerValue2];
@@ -120,6 +124,7 @@ contract.only('Token', (accounts) => {
         assert.equal(balance2.toString(10), web3.toWei("1000000", "ether")); // 0 + 1,000,000
         assert.equal(balance3.toString(10), web3.toWei("1000000", "ether")); // 2,000,000 - 1,000,000
 
+        // Part 2 (same taker)
         const orderHash2 = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, makerValue2, takerValue3);
         const orderValues2 = [makerValue2, takerValue3, takerValue3];
         await arethaFrankloans.approve(dexchange.address, makerValue2, { from: maker });
@@ -142,7 +147,8 @@ contract.only('Token', (accounts) => {
         assert.equal(balance3.toString(10), web3.toWei("500000", "ether")); // 1,000,000 - 500,000
     });
     
-    it('succesfully executes a valid partial order, different takers', async () => {
+    it('executes a valid partial order, different takers', async () => {
+        // Setup
         const maker = accounts[0];
         const taker1 = accounts[1];
         const taker2 = accounts[2];
@@ -166,6 +172,7 @@ contract.only('Token', (accounts) => {
             sender: taker1,
         });
 
+        // Part 1
         const orderHash1 = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, makerValue1, takerValue1);
         const orderAddresses = [maker, taker1, makerToken, takerToken];
         const orderValues1 = [makerValue1, takerValue1, takerValue2];
@@ -186,6 +193,7 @@ contract.only('Token', (accounts) => {
         assert.equal(balance2.toString(10), web3.toWei("1000000", "ether")); // 0 + 1,000,000
         assert.equal(balance3.toString(10), web3.toWei("1000000", "ether")); // 2,000,000 - 1,000,000
 
+        // Part 2 (different taker)
         const orderHash2 = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, makerValue2, takerValue3);
         const orderAddresses2 = [maker, taker2, makerToken, takerToken];
         const orderValues2 = [makerValue2, takerValue3, takerValue3];
@@ -213,12 +221,94 @@ contract.only('Token', (accounts) => {
         assert.equal(balance5.toString(10), web3.toWei("500000", "ether")); // 1,000,000 - 500,000
     });
 
+    it('reverts when taker tries to take tokens from already-completed partial order', async () => {
+        // Setup
+        const maker = accounts[0];
+        const taker = accounts[1];
+        const makerToken = arethaFrankloans.address;
+        const takerToken = andollarsPaak.address;
+        const makerValue1 = web3.toWei("900000", "ether");
+        const makerValue2 = web3.toWei("300000", "ether");
+        const takerValue1 = web3.toWei("1500000", "ether");
+        const takerValue2 = web3.toWei("1000000", "ether");
+        const takerValue3 = web3.toWei("500000", "ether");
+        const makerSignature = ecdsaApi.signMessageComplex({
+            callingAddress: dexchange.address,
+            value: makerValue1,
+            nonce: 0,
+            sender: maker,
+        });
+        const takerSignature = ecdsaApi.signMessageComplex({
+            callingAddress: dexchange.address,
+            value: takerValue2,
+            nonce: 0,
+            sender: taker,
+        });
+
+        // Part 1
+        const orderHash1 = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, makerValue1, takerValue1);
+        const orderAddresses = [maker, taker, makerToken, takerToken];
+        const orderValues1 = [makerValue1, takerValue1, takerValue2];
+        await dexchange.assembleOrder(
+            orderAddresses, orderValues1,
+            makerSignature.r, makerSignature.s, makerSignature.v,
+            takerSignature.r, takerSignature.s, takerSignature.v,
+            false, orderHash1,
+        );
+        await dexchange.fillOrdersTemp(0);
+        
+        let balance0 = await arethaFrankloans.balanceOf(accounts[0]);
+        let balance1 = await arethaFrankloans.balanceOf(accounts[1]);
+        let balance2 = await andollarsPaak.balanceOf(accounts[0]);
+        let balance3 = await andollarsPaak.balanceOf(accounts[1]);
+        assert.equal(balance0.toString(10), web3.toWei("400000", "ether")); // 100,000 - 60,000
+        assert.equal(balance1.toString(10), web3.toWei("600000", "ether")); // 0 + 60,000
+        assert.equal(balance2.toString(10), web3.toWei("1000000", "ether")); // 0 + 1,000,000
+        assert.equal(balance3.toString(10), web3.toWei("1000000", "ether")); // 2,000,000 - 1,000,000
+
+        // Part 2
+        const orderHash2 = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, makerValue2, takerValue3);
+        const orderValues2 = [makerValue2, takerValue3, takerValue3];
+        await arethaFrankloans.approve(dexchange.address, makerValue2, { from: maker });
+        await andollarsPaak.approve(dexchange.address, takerValue3, { from: taker });
+        await dexchange.assembleOrder(
+            orderAddresses, orderValues2,
+            0, 0, 0,
+            0, 0, 0,
+            true, orderHash2,
+        );
+        await dexchange.fillOrdersTemp(1);
+        
+        balance0 = await arethaFrankloans.balanceOf(accounts[0]);
+        balance1 = await arethaFrankloans.balanceOf(accounts[1]);
+        balance2 = await andollarsPaak.balanceOf(accounts[0]);
+        balance3 = await andollarsPaak.balanceOf(accounts[1]);
+        assert.equal(balance0.toString(10), web3.toWei("100000", "ether")); // 40,000 - 30,000
+        assert.equal(balance1.toString(10), web3.toWei("900000", "ether")); // 60,000 + 30,000
+        assert.equal(balance2.toString(10), web3.toWei("1500000", "ether")); // 1,000,000 + 500,000
+        assert.equal(balance3.toString(10), web3.toWei("500000", "ether")); // 1,000,000 - 500,000
+
+        // Part 3 (at this point the order should not have any more tokens to give to any party)
+        const orderHash3 = ecdsaApi.encodeAndHashOrder(maker, makerToken, takerToken, 0, 0);
+        const orderValues3 = [web3.toWei("100000", "ether"), web3.toWei("200000", "ether"), web3.toWei("200000", "ether")];
+        await arethaFrankloans.approve(dexchange.address, web3.toWei("100000", "ether"), { from: maker });
+        await andollarsPaak.approve(dexchange.address, web3.toWei("200000", "ether"), { from: taker });
+        await dexchange.assembleOrder(
+            orderAddresses, orderValues3,
+            0, 0, 0,
+            0, 0, 0,
+            true, orderHash3,
+        );
+        await exceptions.catchRevert(dexchange.fillOrdersTemp(2));
+    });
+
     // it.only('succesfully executes a non-partial order cancellation', async () => {
     // });
+
     // it.only('succesfully executes a partial order cancellation', async () => {
     // });
     
-    it('unauthorised order causes revert', async () => {
+    it('reverts for unauthorised order', async () => {
         const maker = accounts[0];
         const taker = accounts[1];
         const makerToken = arethaFrankloans.address;
