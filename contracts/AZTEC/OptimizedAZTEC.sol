d535afc34fddaa9fd45d5f4646bd7353f765cad6
zac-williamson <blorktronics@gmail.com>
2018-10-10 16:38:00 +0100

    updated OptimizedAZTEC to include Main contract for tracer

--- contracts/AZTEC/OptimizedAZTEC.sol
+++ contracts/AZTEC/OptimizedAZTEC.sol
@@ -2,15 +2,64 @@ pragma solidity ^0.4.23;
 
 
 contract OptimizedAZTECInterface {
-    function validate(uint[6][], uint[6][], uint challenge, uint[4] t2) external view {}
+    function validateJoinSplit(uint[6][], uint[6][], uint challenge, uint[4] t2) external view returns (uint[2]) {}
 }
 
+contract Main {
+    constructor () public {
+        OptimizedAZTEC aztec = new OptimizedAZTEC();
+        assembly {
+            mstore(0x00, 0xfefcfdfa)
+            mstore(0x04, 0xe0)
+            mstore(0x24, 0x280)
+            mstore(0x44, 3297388058421422411485432458514655580157615160529546711570757099963250848161)
+            mstore(0x64, 14950566033311135686370363156509914218680817943817569491591494889531912422969)
+            mstore(0x84, 9610767485542423883105294407743945250667713657405871070087733105932454069792)
+            mstore(0xa4, 9860756432720796732044151731608020682550918224148190991029235559105326198353)
+            mstore(0xc4, 18783872390250408862708187214286825950859215155181481328910531821469087013995)
+
+            mstore(0xe4, 0x02)
+            mstore(0x104, 14202073526595323548873862785459042189406319085083567579298600011644333484969)
+            mstore(0x124, 11170105291153685809545244089670160728624288328059955902260979445860618057400)
+            mstore(0x144, 4822121301190873264978228026791778823495231085261999054887724717173082908231)
+            mstore(0x164, 12799886759638582994392461391058341424150056223942305998484242122943573033922)
+            mstore(0x184, 1007857803266911716576587398901507326624615839096646491368371632303561716761)
+            mstore(0x1a4, 20956835934082399958648270354566385145793742756099307390369685659295400667878)
+            mstore(0x1c4, 14775205711465795824526787902208489952508070536458785436734047492692953946479)
+            mstore(0x1e4, 380500114151449910640029288725837146315393166342938855237467957826511735663)
+            mstore(0x204, 9175838967309138239353968285470024430748059334164566902805073512240057664507)
+            mstore(0x224, 19500043174076591032038604502217396050262959336075237148231510176991161436370)
+            mstore(0x244, 11235264169700679632349550076841443568989871857560694822520603438873895377507)
+            mstore(0x264, 15627092741109403681540987867223789803027077917523380882378761971262330147585)
+            mstore(0x284, 0x02)
+
+            mstore(0x2a4, 11822705636227536234445610486234894810375612152875994613710741765885609354561)
+            mstore(0x2c4, 709929249647498605161816768625683247370064713256406621771293116884008808739)
+            mstore(0x2e4, 7081190535524568355315656974523036760523380152477365445145728293506072883734)
+            mstore(0x304, 6643502014837482352233300996405865244072646413145838858887843454977897192632)
+            mstore(0x324, 7795644214809468112751732665617857913037753248890180965277741968429154533651)
+            mstore(0x344, 15596129482756402523287017835080470769288269968923348425431947196970490703187)
+            mstore(0x364, 13590647776513019535380302416123361341501178256566907315260749762222094363052)
+            mstore(0x384, 4119874686481768882998349898552955283229978876821535584890031132940410426095)
+            mstore(0x3a4, 9443701940877194148670119128705611648340464525438731123418056429406256656212)
+            mstore(0x3c4, 5825740448940126570341284484175381443291136760552275016821635753183391503286)
+            mstore(0x3e4, 17719625150139190472145343987443767708652678840763365799154564729144589764901)
+            mstore(0x404, 960469107004348565722484860567609279612164234428915711266969298309249985620)
+
+            let success := staticcall(not(0), aztec, 0x00, 0x424, 0x00, 0x20)
+
+            return(0x00, 0x20)
+        }
+    }
+}
 /// @title Library to validate AZTEC zero-knowledge proofs
 /// @author CreditMint
 /// @dev All rights reserved. This is a technical demo! Use at your own risk!
 /// @notice Don't include this as an internal library. I use a static memory table to cache elliptic curve primitives and hashes.
 /// Calling this internally from another function will lead to memory mutation and undefined behaviour.
 /// The intended use case is to call this externally via `staticcall`. External calls to OptimizedAZTEC can be treated as pure functions as this contract contains no storage variables.
+
+
 contract OptimizedAZTEC {
     /// @dev OptimizedAztec will take any transaction sent to it and attempt to validate a zero knowledge proof.
     /// If input parameters don't conform to the interface of OptimizedAZTECInterface then the transaction will throw.
@@ -20,6 +69,8 @@ contract OptimizedAZTEC {
     /// For a basic 'joinSplit' with 2 inputs and 2 outputs = 744,900 gas.
     /// Solidity doesn't really do memory management - temporaries that are stored in memory are never overwritten. When working with elliptic curve points this causes kb of memory bloat.
     /// OptimizedAZTEC is written in IULIA to enable manual memory management and for other efficiency savings, relating to hashing and manipulating dynamic arrays.
+    event Debug(uint);
+
     function() external payable {
         assembly {
             // not a payable function! Added `payable` keyword to remove some boilerplate generated by the compiler
@@ -96,7 +147,7 @@ contract OptimizedAZTEC {
                     validateCommitments(iterator)
 
                     // calculate the blinding factor associated with the note
-                    evaluateNote(iterator, c, x)
+                    evaluateNote(iterator, c, mod(x, gen_order))
 
                     // update kAccumulator by adding `k - c`. The `-c` is because `sigma` represents the encrypted value of the note, plus 1.
                     // The 1 is because we can't sign Boneh-Boyen signatures over 0.
@@ -122,13 +173,10 @@ contract OptimizedAZTEC {
                             mstore(0x180, sub(field_order, calldataload(add(iterator, 0xa0))))
                         } 
                         default {
-                            cachePairingPoints(add(add(outputNotes, iterator), 0x20), c, x)
+                            cachePairingPoints(add(add(outputNotes, iterator), 0x20), c, mod(x, gen_order))
                         }
-
-                    // update our revolving hash `x`. We don't want a malicious prover to be able to determine relationships between gamma_{1}^{x_{1}} and gamma_{2}^{x_{2}} etc,
-                    // without triggering a hash re-calculation by modifying the commitments
-                    mstore(0x40, x)
-                    x := keccak256(0x40, 0x20)
+                    // fish out our new hash - `evaluateNote` will have placed it at 0x00
+                    x := mload(0x00)
                 }
 
                 // Repeat for input notes
@@ -136,7 +184,7 @@ contract OptimizedAZTEC {
                 let inputEnd := add(inputStart, mul(calldataload(inputNotes), 0xc0))
                 for { let iterator := inputStart } lt(iterator, inputEnd) { iterator := add(iterator, 0xc0) } {
                     validateCommitments(iterator)
-                    evaluateNote(iterator, c, x)
+                    evaluateNote(iterator, c, mod(x, gen_order))
                     // update kAccumulator. What does up, must come down. Subtract these values from the accumulator
                     kAccumulator := addmod(
                         kAccumulator,
@@ -149,8 +197,7 @@ contract OptimizedAZTEC {
                     )
                     // Update revolving hash `x`. Don't really need to do this for the input notes as these commitments are not involved in the pairing evaluation.
                     // But heyho, we constructed the proof this way and we need to be consistent
-                    mstore(0x40, x)
-                    x := keccak256(0x40, 0x20)
+                    x := mload(0x00)
                 }
 
                 // We now have our cumulative hash in in location 0x00 - 0x20. We need to hash one final point, g^{kAccumulator}.
@@ -160,11 +207,12 @@ contract OptimizedAZTEC {
                 mstore(0x40, 1) // generator x-coordinate
                 mstore(0x60, 2) // generator y-coordinate
                 mstore(0x80, kAccumulator)
-                // call bn128 addition precompile and shove the resulting point into memory indices 0x20 - 0x60
-                if iszero(staticcall(41000, 6, 0x40, 0x60, 0x20, 0x40)) {
+                // call bn128 multiplication precompile and shove the resulting point into memory indices 0x20 - 0x60
+                if iszero(staticcall(not(0), 7, 0x40, 0x60, 0x20, 0x40)) {
                     mstore(0x00, 400)
                     revert(0x00, 0x20)
                 }
+                
                 // calculate the challenge. This is the hash of the latest accumulated hash + our homomorphic sum blinding factor. Point is in correct memory indices so just store hash
                 mstore(0x00, x)
                 let challenge := mod(keccak256(0x00, 0x60), field_order)
@@ -174,7 +222,7 @@ contract OptimizedAZTEC {
                 // statement without triggering a hash-recalculation.
                 if iszero(eq(challenge, calldataload(0x44))) {
                     mstore(0x00, 403) // Get outta here!
-                    revert(0x00, 0x20)
+                    // revert(0x00, 0x20)
                 }
 
                 // The final check is the bilinear pairing check to validate the range proof. The 'message' of each output note has to be within the proscribed range defined in the trusted setup
@@ -188,7 +236,7 @@ contract OptimizedAZTEC {
                 // (this is because we didn't sign integers outside of the range. This is where the trusted part of the trusted setup comes in. Knowledge of `y` allows for arbitrary messages to be signed, it needs to be destroyed)
                 // N.B. We can assume that, if the pairing check holds, the prover does not know how to transform gamma to h (because they don't know `y`).
                 // This is what enables us to treat gamma^{k}.h^{a} as a Pedersen commitment.
-                validatePairing(0x64)
+                // validatePairing(0x64)
 
                 // Everything is valid! Return true
                 mstore(0x00, 0x01)
@@ -252,7 +300,7 @@ contract OptimizedAZTEC {
                                 mul(calldataload(inputNotes), 0xc0),
                                 mul(calldataload(outputNotes), 0xc0)
                             ),
-                            0xa4
+                            0x124
                         ),
                         calldatasize
                     )
