6eb7456a0b73e969f8378ca9d5dce3433ae362b1
Arnaud Schenk <arnaud@creditmint.io>
2018-11-13 14:15:32 +0000

    make path to database relative

--- zk-crypto-js/setup/setup.js
+++ zk-crypto-js/setup/setup.js
@@ -1,15 +1,19 @@
 const BN = require('bn.js');
 const fs = require('fs');
+const path = require('path');
 
 const setup = {};
 const { SIGNATURES_PER_FILE } = require('../params');
 const utils = require('../utils/utils');
 
+const partialPath = path.posix.resolve(__dirname, '../../zk-crypto-cpp/setupDatabase');
+
 setup.readSignature = (inputValue) => {
     const value = Number(inputValue);
     return new Promise((resolve, reject) => {
         const fileNum = Math.ceil(Number(value + 1) / SIGNATURES_PER_FILE);
-        const fileName = `../zk-crypto-cpp/setupDatabase/data${(((fileNum) * SIGNATURES_PER_FILE) - 1)}.dat`;
+
+        const fileName = path.posix.resolve(partialPath, `data${(((fileNum) * SIGNATURES_PER_FILE) - 1)}.dat`);
         fs.readFile(fileName, (err, data) => {
             if (err) {
                 return reject(err);
