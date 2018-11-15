78a4a55a5c2ffe585e77f6836f4776c9617d3f8d
Arnaud Schenk <arnaud@creditmint.io>
2018-11-15 19:09:24 +0000

    make confidentialTransaction only take one argument, modify ZEthereum to deconstruct bytes

--- contracts/AZTEC/ZEthereum.sol
+++ contracts/AZTEC/ZEthereum.sol
@@ -4,6 +4,8 @@ import "./AZTEC.sol";
 import "../ERC20/ERC20Mintable.sol";
 import "../ERC20/SafeMath.sol";
 
+
+// [["0x01","0x02","0x03","0x04","0x05","0x06"]], [["0x0a", "0x0b", "0x0c"]], 123, 456
 /// @title Contract that uses the AZTEC protocol to create Zero-knowledge Ethereum, a confidential representation of Ethereum!
 /// @author Zachary Williamson, CreditMint
 /// @dev All rights reserved. This is a technical demo! Use at your own risk!
@@ -20,6 +22,15 @@ contract ZEthereum {
     event Created(bytes32 domainHash, address contractAddress);
     event ConfidentialTransaction(uint value, bytes metadata);
     event Debug(uint val);
+    event DebugBytes32(bytes32 val);
+    event DebugNotes(bytes notes);
+
+    struct Proof {
+        bytes32[6][] notes;
+        bytes32[3][] inputSignatures;
+        uint m;
+        uint challenge;
+    }
     /// @dev Set the trusted setup public key, the address of the AZTEC verification smart contract and the ERC20 token we're linking to
     constructor(bytes32[4] _setupPubKey) public {
         setupPubKey = _setupPubKey;
@@ -92,6 +103,51 @@ contract ZEthereum {
         noteRegistry[noteHash] = owner;
     }
 
+    function extractDataFromProof(bytes proof) internal returns (Proof){
+        bytes32 offsetToProofData;
+        bytes32 proofBytes;
+        bytes32 offsetToNotes;
+        bytes32 bytesNotes;
+        uint lengthOfNotes;
+        bytes32 bytesInputSignatures;
+        bytes32 offsetToInputSignatures;
+        uint lengthOfInputSignatures;
+
+        uint m;
+        uint challenge;
+
+        assembly {
+            offsetToProofData := calldataload(0x04)
+            proofBytes := add(offsetToProofData, 0x24)
+
+            offsetToNotes := calldataload(proofBytes)
+            m := calldataload(add(proofBytes, 0x20))
+            challenge := calldataload(add(proofBytes, 0x40))
+            offsetToInputSignatures := calldataload(add(proofBytes, 0x60))
+            
+            bytesNotes := add(offsetToNotes, proofBytes)
+            lengthOfNotes := calldataload(bytesNotes)
+
+            bytesInputSignatures := add(offsetToInputSignatures, proofBytes)
+            lengthOfInputSignatures := calldataload(bytesInputSignatures)
+        }
+
+        Proof memory proofData = Proof(new bytes32[6][](lengthOfNotes), new bytes32[3][](lengthOfInputSignatures), m, challenge);
+
+        assembly {
+            // the first mload will get you from the head of the struct to the reference to the length
+            // the reference to the data is 32 bytes after that
+            // mloading that gets you to the start of the actual data in memory
+            let notesDataOffset := mload(add(mload(proofData), 0x20))
+            let inputSignaturesDataOffset := mload(add(mload(add(proofData, 0x20)), 0x20))
+
+            calldatacopy(notesDataOffset, add(0x20, bytesNotes), mul(0xc0, lengthOfNotes))
+            calldatacopy(inputSignaturesDataOffset, add(0x20, bytesInputSignatures), add(0x20, mul(0x60, lengthOfInputSignatures)))
+        }
+
+        return proofData;
+    }
+
     /// @dev validate a confidential transaction. This transaction takes 'm' input notes and 'n - m' output notes (where n = notes.length), along with a public
     /// commitment of ethereum 'kPublic' (located at notes[notes.length][0]). ```notes```, ```m``` and ```challenge``` constitute an AZTEC zero-knowledge
     /// proof that is checked by the AZTEC validator smart contract.
@@ -104,13 +160,14 @@ contract ZEthereum {
     /// If a satisfying zero-knowledge proof is provided, then we know with confidence that the values encrypted by the output notes sum to 10,000,
     /// but we have no idea how those tokens are distributed across the notes.
     /// Each output note has a value bounded by the AZTEC commitment scheme's range proof.
-    function confidentialTransaction(bytes32[6][] notes, uint m, uint challenge, bytes32[3][] inputSignatures, address[] outputOwners, bytes metadata) external payable {
-        require(inputSignatures.length == m, "input signature length invalid");
-        require(inputSignatures.length + outputOwners.length == notes.length, "array length mismatch");
-        uint kPublic = uint(notes[notes.length - 1][0]);
+    function confidentialTransaction(bytes proof, address[] outputOwners, bytes metadata) external payable {
+        Proof memory proofData = extractDataFromProof(proof);
+        
+        require(proofData.inputSignatures.length == proofData.m, "input signature length invalid");
+        require(proofData.inputSignatures.length + outputOwners.length == proofData.notes.length, "array length mismatch");
+        uint kPublic = uint(proofData.notes[proofData.notes.length - 1][0]);
 
-        emit Debug(kPublic);
-        require(AZTECInterface.validateJoinSplit(notes, m, challenge, setupPubKey), "proof not valid!");
+        require(AZTECInterface.validateJoinSplit(proofData.notes, proofData.m, proofData.challenge, setupPubKey), "proof not valid!");
 
         if (kPublic > GROUP_MODULUS_BOUNDARY) {
             // if value > group modulus boundary, this represents a commitment of a public value into confidential note form.
@@ -122,16 +179,16 @@ contract ZEthereum {
             globalSupply = globalSupply.add((GROUP_MODULUS - kPublic) * ZETHEREUM_SCALING_PARAMETER);
         }
 
-        for (uint i = 0; i < m; i++) {
+        for (uint i = 0; i < proofData.m; i++) {
             // call validateInputNote to check that the note exists and that we have a matching signature over the note.
             // pass domainHash in as a function parameter to prevent multiple sloads
             // this will remove the input notes from noteRegistry
-            validateInputNote(notes[i], inputSignatures[i], challenge, domainHash);
+            validateInputNote(proofData.notes[i], proofData.inputSignatures[i], proofData.challenge, domainHash);
         }
-        for (i = 0; i < notes.length - m; i++) {
+        for (i = 0; i < proofData.notes.length - proofData.m; i++) {
             // validate that output notes, attached to the specified owners do not exist in noteRegistry.
             // if all checks pass, add notes into note registry
-            validateOutputNote(notes[i + m], outputOwners[i]);
+            validateOutputNote(proofData.notes[i + proofData.m], outputOwners[i]);
         }
         if (kPublic < GROUP_MODULUS_BOUNDARY) {
             // if value < the group modulus boundary then this public value represents a conversion from confidential note form to public form
