05ce864d07f93835a6f0ebcd7249b21d2cb925d5
zac-williamson <blorktronics@gmail.com>
2018-11-29 11:05:09 +0000

    changed secp256k1.accountFromPrivateKey
    return value 'publicKey' conforms to elliptic.js key spec

--- zk-crypto-js/secp256k1/secp256k1.js
+++ zk-crypto-js/secp256k1/secp256k1.js
@@ -28,12 +28,12 @@ function Secp256k1() {
     curve.accountFromPrivateKey = function accountFromPrivateKey(privateKey) {
         const buffer = Buffer.from(privateKey.slice(2), 'hex');
         const ecKey = curve.keyFromPrivate(buffer);
-        const publicKey = `0x${ecKey.getPublic(false, 'hex').slice(2)}`;
+        const publicKey = `0x${ecKey.getPublic(false, 'hex').slice(2)}`; // remove elliptic.js encoding byte
         const publicHash = web3Utils.sha3(publicKey);
         const address = web3Utils.toChecksumAddress(`0x${publicHash.slice(-40)}`);
         return {
             privateKey,
-            publicKey,
+            publicKey: `0x${ecKey.getPublic(false, 'hex')}`,
             address,
         };
     };
