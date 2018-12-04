dd0683a342c076359bacc931b2be77728dc30200
thomas-waite <thomas.waite4@gmail.com>
2018-12-04 16:49:35 +0000

    Added doxygen comments and removed extra new lines

--- contracts/AZTEC/DOORBELL.sol
+++ contracts/AZTEC/DOORBELL.sol
@@ -1,22 +1,18 @@
 pragma solidity ^0.4.23;
 
 
-/// @title Smart contract to extract public keys from Ethereum addresses
-/// @author CreditMint
-/// @dev All rights reserved
+/// @title Smart contract record the blockNumber alongside a user Ethereum address
+/// @author AZTEC
+/// @dev This is a smart contract whose purpose is to create a public mapping between a user's Ethereum address and 
+/// the number of the block in which a transaction they sent is stored
 contract Doorbell {
 
     mapping(address => uint) public addressBlockMap;
 
     function setBlock() external {
+        /// @title Set the blockNumber in the public mapping between address and blockNumber
+        /// @author AZTEC
         uint number = block.number; 
-
         addressBlockMap[msg.sender] = number;        
     }
 }
-
-    
-
-
-    
-
