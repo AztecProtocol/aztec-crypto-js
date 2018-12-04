d692e0083ba5973583c97a3de1c8ce8e553682f5
thomas-waite <thomas.waite4@gmail.com>
2018-12-04 17:05:36 +0000

    Added an error throw statement, to highlight if an address hasn't rung

--- doorbell/extractPublicKey.js
+++ doorbell/extractPublicKey.js
@@ -3,12 +3,13 @@ const ethUtils = require('ethereumjs-util');
 const helpers = require('../helpers/extractHelpers');
 
 
-async function extractPublicKey(userAddress, contractInstance) {
-    // Extract the block number
-    const extractedNumber = await contractInstance.methods.addressBlockMap(userAddress).call();
+async function extractPublicKey(userAddress, doorbellAddress) {
+    const extractedBlockNumber = await contractInstance.methods.addressBlockMap(userAddress).call();
 
-    // Extract the array of transactions stored in the block
-    const transactionArray = await helpers.blockTxList(extractedNumber);
+    if (extractedBlockNumber === 0) {
+        throw new Error('The Ethereum address in question has not rung Doorbell.sol');
+    }
+    const transactionArray = await helpers.getTransactionHashesFromBlock(extractedBlockNumber);
 
     // Get the ecdsa parameters and appropriate transaction hash
     const [ecdsaParams, returnTx] = await helpers.getECDSAParams(transactionArray, userAddress);
