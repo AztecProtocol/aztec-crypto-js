b0baa7b1f187d114bdb985fc85d283afcdaef66e
thomas-waite <thomas.waite4@gmail.com>
2018-12-12 17:21:36 +0000

    Implemented the doorbell deployer - has deployDoorbell and updateDoorbell methods
    Replicated from the erc20 script

---
+++ zk-crypto-js/demo/deployer/doorbell/doorbell.js
@@ -0,0 +1,50 @@
+const deployer = require('../deployer');
+const transactions = require('../transactions/transactions');
+
+const db = require('../../db/db');
+
+const Doorbell = require('../../../../build/contracts/doorbell.json');
+
+const { web3 } = deployer;
+
+const doorbell = {};
+
+doorbell.deployDoorbell = async (address) => {
+    const wallet = db.wallets.get(address);
+    const doorbellDb = db.contracts.doorbell.get();
+
+    const doorbellContract = new web3.eth.Contract(Doorbell.abi);
+
+    if (doorbellDb && doorbellDb.latest.bytecode === Doorbell.bytecode) {
+        throw new Error('doorbell contract already deployed at address ', doorbellDb.latest.address);
+    }
+    const transactionHash = await deployer.deployContract(doorbellContract, wallet, Doorbell.bytecode);
+    db.transactions.create({
+        status: 'SENT',
+        type: 'CREATE_doorbell',
+        transactionHash,
+    });
+    db.contracts.doorbell.create({
+        bytecode: Doorbell.bytecode,
+        contractAddress: '',
+        transactionReceipt: {},
+        transactionHash,
+    });
+    return transactionHash;
+};
+
+doorbell.updateDoorbell = async (transactionHash) => {
+    const transactionReceipt = await transactions.getTransactionReceipt(transactionHash);
+    return db.contracts.doorbell.update(transactionHash, {
+        bytecode: Doorbell.bytecode,
+        contractAddress: transactionReceipt.contractAddress,
+        transactionReceipt,
+        transactionHash,
+    });
+};
+
+doorbell.contract = (contractAddress) => {
+    return new web3.eth.Contract(Doorbell.abi, contractAddress);
+};
+
+module.exports = doorbell;
